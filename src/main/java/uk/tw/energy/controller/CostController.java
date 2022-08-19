0f504f38fc8c4d9c2461177b48ac1788a46eb633
Kieran Murphy <kimurphy@thoughtworks.com>
2022-08-19 07:57:20 -0500

    2345 | Kieran, Brandon | adds failing test for has meter reading

--- src/main/java/uk/tw/energy/controller/CostController.java
+++ src/main/java/uk/tw/energy/controller/CostController.java
@@ -6,10 +6,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.tw.energy.domain.UsageCost;
+import uk.tw.energy.service.MeterReadingService;
+import uk.tw.energy.service.PricePlanService;
 
 @RestController
 @RequestMapping("/cost/{smartMeterId}")
 public class CostController {
+
+    private PricePlanService pricePlanService;
+
+    public CostController(PricePlanService pricePlanService) {
+        this.pricePlanService = pricePlanService;
+    }
+
     @GetMapping("")
     public ResponseEntity getCost(@PathVariable String smartMeterId) {
         return ResponseEntity.ok(new UsageCost());
