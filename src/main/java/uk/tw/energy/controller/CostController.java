b14ee57194bcd8658521f0969cd7af1cf3c5c9d2
Brandon Byars <brandon.byars@gmail.com>
2022-08-19 08:23:27 -0500

    2345 | Kieran and Brandon | Implemented simple cost calculation

--- src/main/java/uk/tw/energy/controller/CostController.java
+++ src/main/java/uk/tw/energy/controller/CostController.java
@@ -6,21 +6,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.tw.energy.domain.UsageCost;
+import uk.tw.energy.service.AccountService;
 import uk.tw.energy.service.MeterReadingService;
 import uk.tw.energy.service.PricePlanService;
+
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cost/{smartMeterId}")
 public class CostController {
 
+    private AccountService accountService;
     private PricePlanService pricePlanService;
 
-    public CostController(PricePlanService pricePlanService) {
+    public CostController(AccountService accountService, PricePlanService pricePlanService) {
+        this.accountService = accountService;
         this.pricePlanService = pricePlanService;
     }
 
     @GetMapping("")
     public ResponseEntity getCost(@PathVariable String smartMeterId) {
-        return ResponseEntity.ok(new UsageCost());
+        String pricePlanId = accountService.getPricePlanIdForSmartMeterId(smartMeterId);
+        Optional<Map<String, BigDecimal>> consumptionCostPerPricePlan = pricePlanService.getConsumptionCostOfElectricityReadingsForEachPricePlan(smartMeterId);
+        if (consumptionCostPerPricePlan.isPresent()) {
+            BigDecimal consumptionCost = consumptionCostPerPricePlan.get().get(pricePlanId);
+            UsageCost usageCost = new UsageCost(consumptionCost);
+            return ResponseEntity.ok(usageCost);
+        }
+        else {
+            return ResponseEntity.ok(new UsageCost(BigDecimal.ZERO));
+        }
     }
 }
--- src/main/java/uk/tw/energy/domain/UsageCost.java
+++ src/main/java/uk/tw/energy/domain/UsageCost.java
@@ -3,7 +3,13 @@ package uk.tw.energy.domain;
 import java.math.BigDecimal;
 
 public class UsageCost {
+    private final BigDecimal cost;
+
+    public UsageCost(BigDecimal consumptionCost) {
+        this.cost = consumptionCost;
+    }
+
     public BigDecimal getCost() {
-        return BigDecimal.ZERO;
+        return cost;
     }
 }
