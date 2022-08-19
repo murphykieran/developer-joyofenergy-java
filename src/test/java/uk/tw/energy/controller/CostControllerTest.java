e38853340de9b2934de81ba77c23c5bef2ddac23
Brandon Byars <brandon.byars@gmail.com>
2022-08-19 08:23:27 -0500

    2345 | Kieran and Brandon | Implemented simple cost calculation

--- src/test/java/uk/tw/energy/controller/CostControllerTest.java
+++ src/test/java/uk/tw/energy/controller/CostControllerTest.java
@@ -5,23 +5,27 @@ import org.springframework.http.ResponseEntity;
 import uk.tw.energy.domain.ElectricityReading;
 import uk.tw.energy.domain.PricePlan;
 import uk.tw.energy.domain.UsageCost;
+import uk.tw.energy.service.AccountService;
 import uk.tw.energy.service.MeterReadingService;
 import uk.tw.energy.service.PricePlanService;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CostControllerTest {
     @Test
     public void shouldReturnNoCostWhenNoReadingsExist() {
+        AccountService accountService = mock(AccountService.class);
         PricePlanService pricePlanService = mock(PricePlanService.class);
-        CostController costController = new CostController(pricePlanService);
+        CostController costController = new CostController(accountService, pricePlanService);
 
         ResponseEntity response = costController.getCost("TEST-SMART-METER");
 
@@ -30,23 +34,27 @@ public class CostControllerTest {
     }
 
     @Test
-    public void shouldReturnCostForReadings() {
+    public void shouldReturnCostForReadingsWithOnePricePlan() {
 
         // given
+        AccountService accountService = mock(AccountService.class);
         String meterId = "meter-id";
-        ElectricityReading reading = new ElectricityReading(Instant.MIN, BigDecimal.ONE);
-        List<ElectricityReading> readings = Collections.singletonList(reading);
+        String planId = "plan-id";
+        when(accountService.getPricePlanIdForSmartMeterId(meterId)).thenReturn(planId);
+        ElectricityReading firstReading = new ElectricityReading(Instant.MIN, BigDecimal.ONE);
+        ElectricityReading secondReading = new ElectricityReading(Instant.MIN.plusSeconds(3600), BigDecimal.ONE);
+        List<ElectricityReading> readings = Arrays.asList(firstReading, secondReading);
         Map<String, List<ElectricityReading>> readingsByMeterId = Collections.singletonMap(meterId, readings);
         MeterReadingService meterReadingService = new MeterReadingService(readingsByMeterId);
 
-        PricePlan pricePlanForMySmartMeter = new PricePlan("My Plan", "My Supplier", BigDecimal.ONE, Collections.EMPTY_LIST);
+        PricePlan pricePlanForMySmartMeter = new PricePlan(planId, "My Supplier", BigDecimal.ONE, Collections.EMPTY_LIST);
         List<PricePlan> pricePlans = Collections.singletonList(pricePlanForMySmartMeter);
         PricePlanService pricePlanService = new PricePlanService(pricePlans, meterReadingService);
 
-        CostController costController = new CostController(pricePlanService);
+        CostController costController = new CostController(accountService, pricePlanService);
 
         // when
-        ResponseEntity response = costController.getCost("TEST-SMART-METER");
+        ResponseEntity response = costController.getCost(meterId);
 
         // then
         UsageCost actualUsageCost = (UsageCost) response.getBody();
