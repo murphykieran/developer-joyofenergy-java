f99f65a79e48dac0d817dd88b3275855840b177f
Kieran Murphy <kimurphy@thoughtworks.com>
2022-08-29 11:47:36 -0500

    2345 | Charith, Kieran | adds new method in PricePlanService, to return cost of prior week usage.  Not implemented yet

--- src/test/java/uk/tw/energy/controller/CostControllerTest.java
+++ src/test/java/uk/tw/energy/controller/CostControllerTest.java
@@ -2,16 +2,12 @@ package uk.tw.energy.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
-import uk.tw.energy.domain.ElectricityReading;
-import uk.tw.energy.domain.PricePlan;
 import uk.tw.energy.domain.UsageCost;
 import uk.tw.energy.service.AccountService;
-import uk.tw.energy.service.MeterReadingService;
 import uk.tw.energy.service.PricePlanService;
 
 import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.*;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -19,9 +15,15 @@ import static org.mockito.Mockito.*;
 public class CostControllerTest {
     @Test
     public void shouldReturnNoCostWhenNoReadingsExist() {
+
         AccountService accountService = mock(AccountService.class);
         PricePlanService pricePlanService = mock(PricePlanService.class);
+
         CostController costController = new CostController(accountService, pricePlanService);
+
+        when(accountService.getPricePlanIdForSmartMeterId(any(String.class))).thenReturn("PLAN-ID");
+        when(pricePlanService.getConsumptionCostForDateRange(any(String.class), any(String.class), any(LocalDate.class)))
+                .thenReturn(BigDecimal.ZERO);
 
         ResponseEntity response = costController.getCost("TEST-SMART-METER");
 
@@ -36,16 +38,16 @@ public class CostControllerTest {
         String meterId = "meter-id";
         String planId = "plan-id";
         BigDecimal expectedConsumptionCost = BigDecimal.ONE;
+        LocalDate startDate = LocalDate.now().minusWeeks(1L);
 
-        Map<String, BigDecimal> consumptionCostOfElectricityReadingsForEachPricePlan =
-                Collections.singletonMap(planId, expectedConsumptionCost);
+        BigDecimal consumptionCostForDateRange = expectedConsumptionCost;
 
         AccountService accountService = mock(AccountService.class);
         PricePlanService pricePlanService = mock(PricePlanService.class);
 
         when(accountService.getPricePlanIdForSmartMeterId(meterId)).thenReturn(planId);
-        when(pricePlanService.getConsumptionCostOfElectricityReadingsForEachPricePlan(meterId))
-                .thenReturn(Optional.of(consumptionCostOfElectricityReadingsForEachPricePlan));
+        when(pricePlanService.getConsumptionCostForDateRange(meterId, planId, startDate))
+                .thenReturn(consumptionCostForDateRange);
 
         CostController costController = new CostController(accountService, pricePlanService);
 
@@ -55,8 +57,5 @@ public class CostControllerTest {
         // then
         UsageCost actualUsageCost = (UsageCost) response.getBody();
         assertEquals(expectedConsumptionCost, actualUsageCost.getCost());
-
-        verify(accountService).getPricePlanIdForSmartMeterId(meterId);
-        verify(pricePlanService).getConsumptionCostOfElectricityReadingsForEachPricePlan(meterId);
     }
 }
